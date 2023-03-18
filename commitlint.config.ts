0e88b1c5df5b319309895bc8f20a913b654c9985
lzdjack <1956252575@qq.com>
2023-03-18 23:54:16 +0800

    test

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -14,12 +14,12 @@ export const Configuration: UserConfig = {
    * Resolve and load conventional-changelog-atom from node_modules.
    * Referenced packages must be installed
    */
-  parserPreset: 'conventional-changelog-atom',
+  // parserPreset: 'conventional-changelog-atom',
   /*
    * Resolve and load @commitlint/format from node_modules.
    * Referenced package must be installed
    */
-  formatter: '@commitlint/format',
+  // formatter: '@commitlint/format',
   /*
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
@@ -55,27 +55,27 @@ export const Configuration: UserConfig = {
   /*
    * Functions that return true if commitlint should ignore the given message.
    */
-  ignores: [(commit) => commit === ''],
+  // ignores: [(commit) => commit === ''],
   /*
    * Whether commitlint uses the default ignore rules.
    */
-  defaultIgnores: true,
+  // defaultIgnores: true,
   /*
    * Custom URL to show upon failure
    */
-  helpUrl:
-    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+  // helpUrl:
+  //   'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
   /*
    * Custom prompt configs
    */
-  prompt: {
-    messages: {},
-    questions: {
-      type: {
-        description: 'please input type:',
-      },
-    },
-  },
+  // prompt: {
+  //   messages: {},
+  //   questions: {
+  //     type: {
+  //       description: 'please input type:',
+  //     },
+  //   },
+  // },
 }
 
 export default Configuration
