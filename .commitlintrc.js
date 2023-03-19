ac21dcfd77e5074ce3b25aa504fd11c7bc7402e6
lzdjack <1956252575@qq.com>
2023-03-19 15:37:48 +0800

    12

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,8 @@
 import { execSync } from 'child_process'
 import fg from 'fast-glob'
 
+console.log('-------')
+
 const getPackages = (packagePath) =>
   fg.sync('*', { cwd: packagePath, onlyDirectories: true })
 
@@ -14,8 +16,6 @@ const scopes = [
   'deploy',
   'other',
 ]
-
-console.log(scopes, '-------')
 
 const gitStatus = execSync('git status --porcelain || true')
   .toString()
@@ -34,7 +34,67 @@ const subjectEnum = gitStatus
 
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
-  rules: {},
+  rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+    /**
+     * type[scope]: [function] description
+     *      ^^^^^
+     */
+    'scope-enum': [2, 'always', scopes],
+    /**
+     * type[scope]: [function] description
+     *
+     * ^^^^^^^^^^^^^^ empty line.
+     * - Something here
+     */
+    'body-leading-blank': [1, 'always'],
+    /**
+     * type[scope]: [function] description
+     *
+     * - something here
+     *
+     * ^^^^^^^^^^^^^^
+     */
+    'footer-leading-blank': [1, 'always'],
+    /**
+     * type[scope]: [function] description [No more than 72 characters]
+     *      ^^^^^
+     */
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      1,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    /**
+     * type[scope]: [function] description
+     * ^^^^
+     */
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'release',
+        'style',
+        'test',
+        'improvement',
+      ],
+    ],
+  },
   prompt: {
     alias: { fd: 'docs: fix typos' },
     messages: {
@@ -106,10 +166,10 @@ module.exports = {
         emoji: ':rewind:',
       },
     ],
-    // defaultScope: scopeEnum,
-    // customScopesAlign: !scopeEnum ? 'top' : 'bottom',
-    // defaultSubject: subjectEnum && `[${subjectEnum}] `,
-    // allowCustomIssuePrefix: false,
-    // allowEmptyIssuePrefix: false,
+    defaultScope: scopeEnum,
+    customScopesAlign: !scopeEnum ? 'top' : 'bottom',
+    defaultSubject: subjectEnum && `[${subjectEnum}] `,
+    allowCustomIssuePrefix: false,
+    allowEmptyIssuePrefix: false,
   },
 }
