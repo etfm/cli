e3ae4f2d5e741623cb3c90b26d113766d908c5b6
lzdjack <1956252575@qq.com>
2023-03-24 11:48:56 +0800

    fix(project): 修复项目引用方式

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,7 +1,7 @@
 import yParser from '../compiled/yargs-parser'
 import crossSpawn from '../compiled/cross-spawn'
 import * as execa from '../compiled/execa'
-// import lodash from '../compiled/'
+import lodash from '../compiled/lodash'
 import npmlog from '../compiled/npmlog'
 import chalk from '../compiled/chalk'
 import glob from '../compiled/glob'
@@ -14,7 +14,7 @@ export {
   yParser,
   crossSpawn,
   execa,
-  // lodash,
+  lodash,
   npmlog,
   isDev,
   log,
